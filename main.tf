b2823944393687ca7df292796531f297742b876a
Mykola Serdiuk <52344283+Rolika4@users.noreply.github.com>
2024-10-11 10:34:39 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,13 @@ provider "aws" {
   region = var.region
 }
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.54.1"
+    }
+}
+
 data "aws_vpcs" "all_vpcs" {}
 
 output "vpc_ids" {
